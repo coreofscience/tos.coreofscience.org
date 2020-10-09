d0b3a7bed24be384871cf8fae6a607f675604348
Oscar Arbelaez <odarbelaeze@gmail.com>
2020-10-09 03:12:02 -0500

    So long styled components

--- src/components/upload/UploadIndicator.tsx
+++ src/components/upload/UploadIndicator.tsx
@@ -1,24 +1,15 @@
 import React, { FC, useContext } from "react";
-import styled from "styled-components";
+import "./UploadIndicator.css";
 
 import FileCard from "./FileCard";
 import FileContext from "../../context/files";
-
-// TODO: Use fluid grid https://gridbyexample.com/examples/example28/
-
-const UploadZone = styled.div`
-  margin-top: 10px;
-  display: grid;
-  grid-gap: 1em;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-`;
 
 interface Props {}
 
 const UploadIndicator: FC<Props> = () => {
   const { files, remove } = useContext(FileContext);
   return (
-    <UploadZone>
+    <div className="uploadIndicator">
       {files.map((file) => (
         <FileCard
           name={file.name}
@@ -30,7 +21,7 @@ const UploadIndicator: FC<Props> = () => {
           key={file.uuid}
         />
       ))}
-    </UploadZone>
+    </div>
   );
 };
 
