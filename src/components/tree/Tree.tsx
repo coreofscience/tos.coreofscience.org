096a69fa9cf239b4be7dd9684297322548e04dd8
Lizet Gomez <algomezb@unal.edu.co>
2020-10-11 14:14:13 -0500

    new changes

--- src/components/tree/Tree.tsx
+++ src/components/tree/Tree.tsx
@@ -1,28 +1,28 @@
-import React, { FC, useCallback, useState, Fragment } from 'react';
-import sortBy from 'lodash.sortby';
+import React, { FC, useCallback, useState, Fragment } from "react";
+import sortBy from "lodash.sortby";
 
-import CopyImage from '../vectors/CopyImage';
-import StarImgage from '../vectors/StarImage';
-import StarOutline from '../vectors/StarOutline';
+import CopyImage from "../vectors/CopyImage";
+import StarImgage from "../vectors/StarImage";
+import StarOutline from "../vectors/StarOutline";
 
-import Reference from './Reference';
-import { Article } from '../../utils/customTypes';
+import Reference from "./Reference";
+import { Article } from "../../utils/customTypes";
 
-import './Tree.css';
-import DATA from './data.json';
+import "./Tree.css";
+import DATA from "./data.json";
 
 const INFO: {
   [key: string]: { title: string; info: string };
 } = {
   root: {
-    title: 'Root',
+    title: "Root",
     info: `
       Here you should find seminal articles from the original articles of
       your topic of interest.
     `,
   },
   trunk: {
-    title: 'Trunk',
+    title: "Trunk",
     info: `
       Here you should find articles where your topic of interest got a
       structure, these should be the first authors to discover the
@@ -30,7 +30,7 @@ const INFO: {
     `,
   },
   leaf: {
-    title: 'Leaves',
+    title: "Leaves",
     info: `
       Here you should find recent articles and reviews that should
       condense very well your topics.
@@ -38,59 +38,37 @@ const INFO: {
   },
 };
 
-const DEFAULT_SHOW = {
-  root: true,
-  trunk: true,
-  leaf: true,
-};
-
 const Tree: FC<{}> = () => {
   const data: { [section: string]: Article[] } = DATA;
   const [star, setStar] = useState<{ [label: string]: boolean }>({});
-  const [show, setShow] = useState<{ [section: string]: boolean }>(
-    DEFAULT_SHOW
-  );
+  const [show, setShow] = useState<"root" | "trunk" | "leaf" | null>(null);
 
   const toggleStar = useCallback((label: string) => {
     setStar((current) => ({ ...current, [label]: !current[label] }));
   }, []);
 
-  const toggleShow = useCallback((label: string) => {
+  const toggleShow = useCallback((label: "root" | "trunk" | "leaf") => {
     setShow((curr) => {
-      if (
-        Object.values(curr).reduce(
-          (acc, selected) => acc + (selected ? 1 : 0),
-          0
-        ) === 1 &&
-        curr[label]
-      ) {
-        return {
-          root: true,
-          trunk: true,
-          leaf: true,
-        };
+      if (curr === label) {
+        return null;
       }
-      return {
-        root: false,
-        trunk: false,
-        leaf: false,
-        [label]: true,
-      };
+      return label;
     });
   }, []);
 
   return (
     <Fragment>
-      <div className='tree-menu'>
+      <div className="tree-menu">
         {Object.entries(data).map(([sectionName, articles]) => (
           <button
             className={`btn btn-${sectionName} ${sectionName} ${
-              show[sectionName] ? 'active' : 'inactive'
+              !show || show === sectionName ? "active" : "inactive"
             }`}
-            title='Show only trunk'
-            onClick={() => toggleShow(sectionName)}
-            key={`menu-${sectionName}`}>
-            <strong>{(INFO[sectionName] || { title: '' }).title}</strong>
+            title="Show only trunk"
+            onClick={() => toggleShow(sectionName as "root" | "trunk" | "leaf")}
+            key={`menu-${sectionName}`}
+          >
+            <strong>{(INFO[sectionName] || { title: "" }).title}</strong>
             <small>{articles.length} articles</small>
           </button>
         ))}
@@ -98,14 +76,15 @@ const Tree: FC<{}> = () => {
 
       {Object.entries(data).map(
         ([sectionName, articles]) =>
-          !!show[sectionName] && (
+          (!show || show === sectionName) && (
             <div
               className={`tree-segment ${sectionName}`}
-              key={`tree-segment-${sectionName}`}>
-              <div className='info'>
-                <h2>{(INFO[sectionName] || { title: '' }).title}</h2>
+              key={`tree-segment-${sectionName}`}
+            >
+              <div className="info">
+                <h2>{(INFO[sectionName] || { title: "" }).title}</h2>
                 <p>
-                  {(INFO[sectionName] || { info: '' }).info}
+                  {(INFO[sectionName] || { info: "" }).info}
                   Here you should find seminal articles from the original
                   articles of your topic of interest.
                 </p>
@@ -113,16 +92,17 @@ const Tree: FC<{}> = () => {
                   <strong>Keywords:</strong> keyword, keyword, keyword
                 </p>
               </div>
-              <div className='articles'>
+              <div className="articles">
                 {sortBy(articles, (article) => star[article.label]).map(
                   (article) => (
-                    <div className='article' key={`article-${article.label}`}>
+                    <div className="article" key={`article-${article.label}`}>
                       <Reference key={article.label} {...article} />
                       <CopyImage />
 
                       <button
-                        className='btn-star'
-                        onClick={() => toggleStar(article.label)}>
+                        className="btn-star"
+                        onClick={() => toggleStar(article.label)}
+                      >
                         {!!star[article.label] ? (
                           <StarImgage />
                         ) : (
