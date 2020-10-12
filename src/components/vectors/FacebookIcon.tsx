f2072c65e26a247d50d5942900c757ede3d0ab3a
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-10-12 17:27:02 -0500

    Refactor svg files and change them color

--- src/components/vectors/FacebookIcon.tsx
+++ src/components/vectors/FacebookIcon.tsx
@@ -1,24 +1,9 @@
 import React from "react";
 
-const FACEBOOK = `
-<svg
-  id="Capa_1"
-  enable-background="new 0 0 512 512"
-  height="512"
-  viewBox="0 0 512 512"
-  width="512"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path d="m512 256c0-141.4-114.6-256-256-256s-256 114.6-256 256 114.6 256 256 256c1.5 0 3 0 4.5-.1v-199.2h-55v-64.1h55v-47.2c0-54.7 33.4-84.5 82.2-84.5 23.4 0 43.5 1.7 49.3 2.5v57.2h-33.6c-26.5 0-31.7 12.6-31.7 31.1v40.8h63.5l-8.3 64.1h-55.2v189.5c107-30.7 185.3-129.2 185.3-246.1z" />
-</svg>
-`;
-
-const FacebookIcon = ({ ...props }) => (
-  <img
-    src={`data:image/svg+xml;base64,${btoa(FACEBOOK)}`}
-    alt="facebook icon"
-    {...props}
-  />
+const FacebookIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256s114.6 256 256 256c1.5 0 3 0 4.5-.1V312.7h-55v-64.1h55v-47.2c0-54.7 33.4-84.5 82.2-84.5 23.4 0 43.5 1.7 49.3 2.5v57.2h-33.6c-26.5 0-31.7 12.6-31.7 31.1v40.8h63.5l-8.3 64.1h-55.2v189.5C433.7 471.4 512 372.9 512 256z" />
+  </svg>
 );
 
 export default FacebookIcon;
