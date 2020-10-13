aaf1cb3048463cb5f8ad03ba001e688f9c35e95b
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-10-12 23:11:09 -0500

    Add the ToS citation articles in the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,6 +9,56 @@ import "./Footer.css";
 
 const Footer = () => (
   <div className="footer">
+    <div>
+      <h4 className="footer__sectionHeader">Citing Tree of Science</h4>
+      <p>
+        If Tree of Science contributes to a project that leads to a scientific
+        publication, please acknowledge this fact by citing:
+        <ul className="footer__sectionLinks citations">
+          <li>
+            <a
+              target="blank"
+              rel="noopener noreferrer"
+              href="https://revistas.unal.edu.co/index.php/ingeinv/article/view/77718/0"
+            >
+              {/* Valencia-Hernandez, D. S., Robledo, S., Pinilla, R., Duque-Méndez,
+              N. D., & Olivar-Tost, G. (2020). Algoritmo SAP para análisis de
+              citaciones: Una mejora al Árbol de la Ciencia. Ingeniería E
+              Investigación, 40(1), 45-49. */}
+              Algoritmo SAP para análisis de citaciones: Una mejora al Árbol de
+              la Ciencia
+            </a>
+          </li>
+          <li>
+            <a
+              target="blank"
+              rel="noopener noreferrer"
+              href="http://www.scielo.org.co/scielo.php?script=sci_arttext&pid=S1794-24702016000100010"
+            >
+              {/* Zuluaga, Martha, Robledo, Sebastian, Osorio-Zuluaga, German A,
+              Yathe, Laura, Gonzalez, Diana, & Taborda, Gonzalo. (2016).
+              Metabolomics and pesticides: systematic literature review using
+              graph theory for analysis of references. Nova, 14(25), 121-138. */}
+              Metabolomics and pesticides: systematic literature review using
+              graph theory for analysis of references
+            </a>
+          </li>
+          <li>
+            <a
+              target="blank"
+              rel="noopener noreferrer"
+              href="https://revistas.udistrital.edu.co/index.php/vinculos/article/view/9664"
+            >
+              {/* Robledo, S., Osorio, G., & Lopez, C. (2014). Networking en pequeña
+              empresa: una revisión bibliográfica utilizando la teoria de
+              grafos. Revista vínculos, 11(2), 6-16. */}
+              Networking en pequeña empresa: una revisión bibliográfica
+              utilizando la teoria de grafos
+            </a>
+          </li>
+        </ul>
+      </p>
+    </div>
     <div className="footer__mainLinks">
       <section className="footer__section">
         <h4 className="footer__sectionHeader">This project on GitHub</h4>
