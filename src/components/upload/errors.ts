33923a26db2a224468b9c97ebe2faa10bfd5a41d
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-10-13 01:01:25 -0500

    Change error message

--- src/components/upload/errors.ts
+++ src/components/upload/errors.ts
@@ -2,7 +2,7 @@ export const MAX_FILE_SIZE = 5; // megabytes
 
 const FileErrorMap = {
   isi: "does not look like an ISI valid file",
-  max_size: `has a size greater than the max allowed: ${MAX_FILE_SIZE} MB`,
+  max_size: `is too big to process (max. ${MAX_FILE_SIZE}MB)`,
 };
 
 export default FileErrorMap;
