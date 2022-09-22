a0c19bad1df5203061acb77917cfec5963ced2d2
Lizet Gomez <lizeth.9515@gmail.com>
2022-09-22 19:34:21 +0100

    add new article to the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -69,6 +69,24 @@ const Footer = () => (
               simple={false}
             />
           </li>
+          <li>
+            <Reference
+              label=""
+              authors={[
+                "Zuluaga, M.",
+                "Robledo, S.",
+                "Arbelaez-Echeverri, O.",
+                "Osorio-Zuluaga, G.A.",
+                "& Duque-Méndez, N.",
+              ]}
+              year={2022}
+              title="Tree of Science - ToS: A web-based tool for scientific literature recommendation. Search less, research more!"
+              journal="Issues in Science and Technology Librarianship"
+              volume="100"
+              doi="10.29173/istl2696"
+              simple={false}
+            />
+          </li>
         </ul>
       </section>
       <section className="footer__section socialLinks">
