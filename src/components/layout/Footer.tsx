5fc516875cae0f6c13aca95bdda91abb27dcb937
Lizet Gomez <lizeth.9515@gmail.com>
2022-09-23 17:42:06 +0100

    add new article to the footer firt

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,6 +18,24 @@ const Footer = () => (
             <Reference
               label=""
               authors={[
+                "Zuluaga, M.",
+                "Robledo, S.",
+                "Arbelaez-Echeverri, O.",
+                "Osorio-Zuluaga, G.A.",
+                "& Duque-Méndez, N.",
+              ]}
+              year={2022}
+              title="Tree of Science - ToS: A web-based tool for scientific literature recommendation. Search less, research more!"
+              journal="Issues in Science and Technology Librarianship"
+              volume="100"
+              doi="10.29173/istl2696"
+              simple={false}
+            />
+          </li>
+          <li>
+            <Reference
+              label=""
+              authors={[
                 "Valencia-Hernandez, D. S.",
                 "Robledo, S.",
                 "Pinilla, R.",
@@ -66,24 +84,6 @@ const Footer = () => (
               issue="2"
               page="6-16"
               doi="10.14483/2322939X.9664"
-              simple={false}
-            />
-          </li>
-          <li>
-            <Reference
-              label=""
-              authors={[
-                "Zuluaga, M.",
-                "Robledo, S.",
-                "Arbelaez-Echeverri, O.",
-                "Osorio-Zuluaga, G.A.",
-                "& Duque-Méndez, N.",
-              ]}
-              year={2022}
-              title="Tree of Science - ToS: A web-based tool for scientific literature recommendation. Search less, research more!"
-              journal="Issues in Science and Technology Librarianship"
-              volume="100"
-              doi="10.29173/istl2696"
               simple={false}
             />
           </li>
