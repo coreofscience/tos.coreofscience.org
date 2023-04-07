bbee0355e09d7e51fb93452efa5939ce70b07c46
Oscar Arbelaez <oscar@arbelaez.dev>
2023-04-07 18:30:29 +0100

    Migrate from cra to vitest

--- src/components/providers/FirebaseProvider.tsx
+++ src/components/providers/FirebaseProvider.tsx
@@ -11,14 +11,14 @@ import FirebaseContext from "../../context/FirebaseContext";
 import { FirebaseContextType } from "../../types/firebaseContext";
 
 const CONFIG = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASEURL,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 const FirebaseProvider: FC<{ children?: ReactNode }> = ({ children }) => {
