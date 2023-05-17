73945ba547427608b097aa272139d8ce4575d836
Daniel S. Valencia H <dsvalenciah+gh@gmail.com>
2023-05-16 23:52:51 -0500

    fix: try to make preview deploy to work

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AppLayout from "./components/layout/AppLayout";
 
-import LogIn from "./components/account/LogIn";
-import SignUp from "./components/account/SignUp";
-
 import FilesProvider from "./components/providers/FilesProvider";
 import { QueryClient, QueryClientProvider } from "react-query";
 import UserProvider from "./components/providers/UserProvider";
@@ -13,6 +10,8 @@ import UserProvider from "./components/providers/UserProvider";
 const Home = React.lazy(() => import("./components/upload/Home"));
 const Result = React.lazy(() => import("./components/tree/Result"));
 const NotFound = React.lazy(() => import("./components/NotFound"));
+const LogIn = React.lazy(() => import("./components/account/LogIn"));
+const SignUp = React.lazy(() => import("./components/account/SignUp"));
 
 const queryClient = new QueryClient();
 
