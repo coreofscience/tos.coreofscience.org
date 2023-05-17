ef575fc43d801936c48d124168587c76be0758c1
Oscar Arbelaez <oscar@arbelaez.dev>
2023-05-17 13:38:59 +0100

    Try that

--- src/components/account/Login/schema.ts
+++
@@ -1,9 +0,0 @@
-import { object, string } from "yup";
-import { errorMessages } from "../common/errorMessages";
-
-export const loginSchema = object()
-  .shape({
-    email: string().required().email(errorMessages.email),
-    password: string().required().min(8, errorMessages.password),
-  })
-  .required();
