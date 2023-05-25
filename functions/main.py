0f581f7fb8d7f118c13d9aa8998dba79db1b6845
Oscar Arbelaez <oscar@arbelaez.dev>
2023-05-25 11:05:56 +0100

    Debug with the local emulators

--- functions/main.py
+++ functions/main.py
@@ -12,8 +12,9 @@ import networkx as nx
 from bibx import Sap, read_any
 from firebase_admin import firestore, initialize_app, storage
 from firebase_functions.firestore_fn import DocumentSnapshot, Event, on_document_created
+from firebase_functions.options import MemoryOption
 
-MAX_SIZE = 10  # MB
+logging.basicConfig(level=logging.INFO)
 
 initialize_app()
 
@@ -53,12 +54,11 @@ def convert_tos_to_json(tree: nx.DiGraph) -> Dict[str, List[Dict]]:
     return output
 
 
-def get_contents(document_data: Dict[str, Any]) -> Dict[str, str]:
+def get_contents(
+    document_data: Dict[str, Any], max_size_megabytes=10
+) -> Dict[str, str]:
     """Get the contents for the files in order to create the graph."""
-    names = [
-        f'isi-files/{name["stringValue"]}'
-        for name in document_data["files"]["arrayValue"]["values"]
-    ]
+    names = [f"isi-files/{name}" for name in document_data["files"]]
     logging.info("Reading source files", extra={"names": names})
     blobs = list(filter(None, [storage.bucket().get_blob(name) for name in names]))
 
@@ -68,7 +68,7 @@ def get_contents(document_data: Dict[str, Any]) -> Dict[str, str]:
         if blob is None:
             continue
         size += blob.size or 0
-        if (size / 1e6) > MAX_SIZE:
+        if (size / 1e6) > max_size_megabytes:
             break
         output[blob.name] = blob.download_as_text()
     return output
@@ -78,44 +78,9 @@ def get_int_utcnow() -> int:
     return int(datetime.utcnow().timestamp())
 
 
-def create_tree_v2(event, context):
-    """Handles new created documents in firestore with path `trees/{treeId}`"""
-    tree_id = context.resource.split("/").pop()
-
-    logging.info(f"Handling new created tree {event} {context} {tree_id}")
-
-    client = firestore.client()
-    document_reference = client.collection("trees").document(tree_id)
-    document_reference.update({"startedDate": get_int_utcnow()})
-
-    try:
-        logging.info("Tree process started")
-        contents = get_contents(event["value"]["fields"])
-        tos = tree_from_strings(list(contents.values()))
-        result = convert_tos_to_json(tos)
-        document_reference.update(
-            {
-                "version": "2",
-                "result": result,
-                "error": None,
-                "finishedDate": get_int_utcnow(),
-            }
-        )
-        logging.info("Tree process finished")
-    except Exception as error:
-        logging.exception("Tree process failed")
-        document_reference.update(
-            {
-                "version": "2",
-                "result": None,
-                "error": str(error),
-                "finishedDate": get_int_utcnow(),
-            }
-        )
-
-
-@on_document_created(document="users/{userId}/trees/{treeId}")
-def process_user_tree(event: Event[DocumentSnapshot | None]) -> None:
+def create_tree_v2(
+    event: Event[DocumentSnapshot | None], max_size_megabytes=10
+) -> None:
     if event.data is None or (data := event.data.to_dict()) is None:
         return
     logging.info(f"handling new created tree {event} {event.data.to_dict()}")
@@ -123,10 +88,10 @@ def process_user_tree(event: Event[DocumentSnapshot | None]) -> None:
     client = firestore.client()
     ref = client.document(event.document)
     ref.update({"startedDate": get_int_utcnow()})
+    logging.info("Tree process started")
 
     try:
-        logging.info("Tree process started")
-        contents = get_contents(data)
+        contents = get_contents(data, max_size_megabytes=max_size_megabytes)
         tos = tree_from_strings(list(contents.values()))
         result = convert_tos_to_json(tos)
         ref.update(
@@ -148,3 +113,11 @@ def process_user_tree(event: Event[DocumentSnapshot | None]) -> None:
                 "finishedDate": get_int_utcnow(),
             }
         )
+
+
+@on_document_created(
+    document="users/{userId}/trees/{treeId}",
+    memory=MemoryOption.MB_512,
+)
+def process_user_tree(event: Event[DocumentSnapshot | None]) -> None:
+    create_tree_v2(event, max_size_megabytes=20)
