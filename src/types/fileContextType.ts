d494b574b479bb0416630ace9780d56c1a3be3e6
Santiago Restrepo <santiagorv246@gmail.com>
2023-09-04 08:18:52 -0500

    Correct the FileContext Interface name

--- src/types/fileContextType.ts
+++ src/types/fileContextType.ts
@@ -1,6 +1,6 @@
 import { FileMetadata } from "./fileMetadata";
 
-export interface FileContexType {
+export interface FileContextType {
   add: (metadata: FileMetadata) => void;
   remove: (hash: string) => void;
   track: (hash: string, progress: number) => void;
