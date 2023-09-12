b0db00b5c42d7ecda6a290885efe7e1073446368
Santiago Restrepo <santiagorv246@gmail.com>
2023-09-12 13:10:23 -0500

    Create TreeMenu component

--- src/components/tree/Tree.tsx
+++ src/components/tree/Tree.tsx
@@ -12,6 +12,7 @@ import CopyImage from "../vectors/CopyImage";
 import Reference from "./Reference";
 import { mostCommon } from "../../utils/arrays";
 
+import TreeMenu from "./TreeMenu";
 import { TreeVis } from "./TreeVis";
 
 import {
@@ -35,20 +36,9 @@ export interface Props {
 
 const Tree: FC<Props> = ({ treeSections, treePath, stars }: Props) => {
   const firebase = useFirebase();
-  const [show, setShow] = useState<
-    | "root"
-    | "trunk"
-    | "branch_type_1"
-    | "branch_type_2"
-    | "branch_type_3"
-    | "leaf"
-    | null
-  >(null);
+  const [show, setShow] = useState<Section | null>(null);
   const [infoEntries, setInfoEntries] = useState<
     [Section, RootInfo | TrunkInfo | LeafInfo | BranchInfo][]
-  >([]);
-  const [branchesEntries, setBranchesEntries] = useState<
-    [string, { id: number; title: string }][]
   >([]);
   const treeDocRef = useMemo(
     () => doc(firebase.firestore, treePath),
@@ -62,14 +52,6 @@ const Tree: FC<Props> = ({ treeSections, treePath, stars }: Props) => {
         RootInfo | TrunkInfo | LeafInfo | BranchInfo
       ][]
     );
-    if (info.branch && treeSections.branch?.length > 0) {
-      setBranchesEntries(
-        Object.entries(info.branch.branches) as [
-          string,
-          { id: number; title: string }
-        ][]
-      );
-    }
   }, []);
 
   const keywords: Keywords = useMemo(() => {
@@ -77,67 +59,22 @@ const Tree: FC<Props> = ({ treeSections, treePath, stars }: Props) => {
       root: [],
       trunk: [],
       leaf: [],
-      branch: {
-        branch_type_1: [],
-        branch_type_2: [],
-        branch_type_3: [],
-      },
+      branch_1: [],
+      branch_2: [],
+      branch_3: [],
     };
     const sections: Section[] = Object.keys(keywords) as Section[];
     for (let section of sections) {
-      if (section !== "branch") {
-        for (let article of treeSections[section]) {
-          if (!article.keywords) continue;
-          keywords[section] = keywords[section].concat(article.keywords);
-        }
-        keywords[section] = mostCommon(
-          keywords[section].map((keyword) => {
-            return keyword.toLowerCase();
-          }),
-          5
-        );
-      } else {
-        const articles = treeSections.branch;
-        if (articles?.length > 0) {
-          for (let article of articles) {
-            if (!article.keywords) continue;
-            if (keywords.branch) {
-              if (article.branch === 1) {
-                keywords.branch.branch_type_1 =
-                  keywords.branch?.branch_type_1.concat(article.keywords);
-              }
-              if (article.branch === 2) {
-                keywords.branch.branch_type_2 =
-                  keywords.branch?.branch_type_2.concat(article.keywords);
-              }
-              if (article.branch === 3) {
-                keywords.branch.branch_type_3 =
-                  keywords.branch?.branch_type_3.concat(article.keywords);
-              }
-            }
-          }
-        }
-        if (keywords.branch) {
-          keywords.branch.branch_type_1 = mostCommon(
-            keywords.branch.branch_type_1.map((keyword) => {
-              return keyword.toLowerCase();
-            }),
-            5
-          );
-          keywords.branch.branch_type_2 = mostCommon(
-            keywords.branch.branch_type_2.map((keyword) => {
-              return keyword.toLowerCase();
-            }),
-            5
-          );
-          keywords.branch.branch_type_3 = mostCommon(
-            keywords.branch.branch_type_3.map((keyword) => {
-              return keyword.toLowerCase();
-            }),
-            5
-          );
-        }
+      for (let article of treeSections[section]) {
+        if (!article.keywords) continue;
+        keywords[section] = keywords[section].concat(article.keywords);
       }
+      keywords[section] = mostCommon(
+        keywords[section].map((keyword) => {
+          return keyword.toLowerCase();
+        }),
+        5
+      );
     }
     return keywords;
   }, [treeSections]);
@@ -174,15 +111,7 @@ const Tree: FC<Props> = ({ treeSections, treePath, stars }: Props) => {
   );
 
   const toggleShow = useCallback(
-    (
-      label:
-        | "root"
-        | "trunk"
-        | "branch_type_1"
-        | "branch_type_2"
-        | "branch_type_3"
-        | "leaf"
-    ) => {
+    (label: Section) => {
       setShow((curr) => {
         if (curr === label) {
           return null;
@@ -195,160 +124,55 @@ const Tree: FC<Props> = ({ treeSections, treePath, stars }: Props) => {
 
   return (
     <Fragment>
-      <div className="tree-menu">
-        {infoEntries.map(([sectionName, info]) =>
-          sectionName !== "branch" ? (
-            <button
-              className={`btn btn-${sectionName} ${sectionName} ${
-                !show || show === sectionName ? "active" : "inactive"
-              }`}
-              title={`Show only ${sectionName}`}
-              onClick={() =>
-                toggleShow(
-                  sectionName as
-                    | "root"
-                    | "trunk"
-                    | "branch_type_1"
-                    | "branch_type_2"
-                    | "branch_type_3"
-                    | "leaf"
-                )
-              }
-              key={`menu-${sectionName}`}
-            >
-              <strong>{(info || { title: "" }).title}</strong>
-              <small>{treeSections[sectionName].length} articles</small>
-            </button>
-          ) : (
-            branchesEntries &&
-            branchesEntries.length > 0 && (
-              <div className="btn-branches" key="section-branches">
-                {branchesEntries.map(([type, branchInfo]) => (
-                  <button
-                    key={`branch-${type}`}
-                    className={`btn btn-branch branch ${
-                      !show || show === type ? "active" : "inactive"
-                    }`}
-                    title={`Show only branch type ${type}`}
-                    onClick={() =>
-                      toggleShow(
-                        type as
-                          | "branch_type_1"
-                          | "branch_type_2"
-                          | "branch_type_3"
-                      )
-                    }
-                  >
-                    <strong>{(branchInfo || { title: "" }).title}</strong>
-                  </button>
-                ))}
-              </div>
-            )
-          )
-        )}
-      </div>
+      <TreeMenu show={show} toggleShow={toggleShow} treeSections={treeSections} info={info}/>
 
       <TreeVis treeResult={treeSections} />
 
-      {infoEntries.map(([sectionName, info]) =>
-        sectionName !== "branch" && (!show || show === sectionName) ? (
-          <div
-            className={`tree-segment ${sectionName}`}
-            key={`tree-segment-${sectionName}`}
-          >
-            <div className="info">
-              <h2>{(info || { title: "" }).title}</h2>
-              <p>{(info || { info: "" }).info}</p>
-              {keywords[sectionName].length > 0 && (
-                <p>
-                  <strong>Keywords:</strong> {keywords[sectionName].join(", ")}
-                </p>
-              )}
-            </div>
-            <div className="articles">
-              {orderBy(
-                treeSections[sectionName].map((article) => {
-                  const labelAsBase64 = encode(article.label);
-                  const star = stars[labelAsBase64] ?? 0;
-                  return { article, labelAsBase64, star };
-                }),
-                "star",
-                "desc"
-              ).map(({ article, labelAsBase64, star }) => (
-                <div className="article" key={`article-${article.label}`}>
-                  <Reference key={article.label} {...article} />
-                  <button
-                    className="btn-copy"
-                    onClick={() => copy(article.label)}
-                  >
-                    <CopyImage />
-                  </button>
-                  <button
-                    className={`btn-star ${star ? "favorite" : ""}`}
-                    onClick={() => toggleStar(labelAsBase64)}
-                  >
-                    <StarImage />
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-        ) : (
-          branchesEntries.map(
-            ([type, branchInfo]) =>
-              sectionName === "branch" &&
-              (!show || show === type) && (
-                <div
-                  className="tree-segment branch"
-                  key={`tree-segment-${type}`}
-                >
-                  <div className="info">
-                    <h2>{(branchInfo || { title: "" }).title}</h2>
-                    <p>{(info || { info: "" }).info}</p>
-                    {keywords.branch && keywords.branch[type].length > 0 && (
-                      <p>
-                        <strong>Keywords:</strong>{" "}
-                        {keywords.branch && keywords.branch[type].join(", ")}
-                      </p>
-                    )}
+      {infoEntries.map(
+        ([sectionName, info]) =>
+          (!show || show === sectionName) && (
+            <div
+              className={`tree-segment ${sectionName}`}
+              key={`tree-segment-${sectionName}`}
+            >
+              <div className="info">
+                <h2>{(info || { title: "" }).title}</h2>
+                <p>{(info || { info: "" }).info}</p>
+                {keywords[sectionName].length > 0 && (
+                  <p>
+                    <strong>Keywords:</strong> {keywords[sectionName].join(", ")}
+                  </p>
+                )}
+              </div>
+              <div className="articles">
+                {orderBy(
+                  treeSections[sectionName].map((article) => {
+                    const labelAsBase64 = encode(article.label);
+                    const star = stars[labelAsBase64] ?? 0;
+                    return { article, labelAsBase64, star };
+                  }),
+                  "star",
+                  "desc"
+                ).map(({ article, labelAsBase64, star }) => (
+                  <div className="article" key={`article-${article.label}`}>
+                    <Reference key={article.label} {...article} />
+                    <button
+                      className="btn-copy"
+                      onClick={() => copy(article.label)}
+                    >
+                      <CopyImage />
+                    </button>
+                    <button
+                      className={`btn-star ${star ? "favorite" : ""}`}
+                      onClick={() => toggleStar(labelAsBase64)}
+                    >
+                      <StarImage />
+                    </button>
                   </div>
-                  <div className="articles">
-                    {treeSections.branch?.length > 0 &&
-                      orderBy(
-                        treeSections.branch
-                          .filter((article) => article.branch === branchInfo.id)
-                          .map((article) => {
-                            const labelAsBase64 = encode(article.label);
-                            const star = stars[labelAsBase64] ?? 0;
-                            return { article, labelAsBase64, star };
-                          }),
-                        "star",
-                        "desc"
-                      ).map(({ article, labelAsBase64, star }) => (
-                        <div
-                          className="article"
-                          key={`article-${article.label}`}
-                        >
-                          <Reference key={article.label} {...article} />
-                          <button
-                            className="btn-copy"
-                            onClick={() => copy(article.label)}
-                          >
-                            <CopyImage />
-                          </button>
-                          <button
-                            className={`btn-star ${star ? "favorite" : ""}`}
-                            onClick={() => toggleStar(labelAsBase64)}
-                          >
-                            <StarImage />
-                          </button>
-                        </div>
-                      ))}
-                  </div>
-                </div>
-              )
+                ))}
+              </div>
+            </div>
           )
-        )
       )}
     </Fragment>
   );
---
+++ src/components/tree/TreeMenu.tsx
@@ -0,0 +1,104 @@
+import {FC} from "react";
+import {Info, Section} from "../../types/treeType";
+import {TreeResult} from "../../types/result";
+
+import "./TreeMenu.css"
+
+type Props = {
+  info: Info;
+  treeSections: TreeResult;
+  show: Section | null;
+  toggleShow: (section: Section) => void;
+}
+
+const TreeMenu: FC<Props> = ({info, treeSections, show, toggleShow}) => {
+  return (
+    <>
+      <div className="tree-menu">
+        <button
+          className={`btn btn-root root ${
+            !show || show === "root" ? "active" : "inactive"
+          }`}
+          title="Show only root"
+          onClick={() =>
+            toggleShow("root" as Section)
+          }
+          key="menu-root"
+        >
+          <strong>{(info.root || { title: "" }).title}</strong>
+          <small>{treeSections.root.length} articles</small>
+        </button>
+
+        <button
+          className={`btn btn-trunk trunk ${
+            !show || show === "trunk" ? "active" : "inactive"
+          }`}
+          title="Show only trunk"
+          onClick={() =>
+            toggleShow("trunk" as Section)
+          }
+          key="menu-trunk"
+        >
+          <strong>{(info.trunk || { title: "" }).title}</strong>
+          <small>{treeSections.trunk.length} articles</small>
+        </button>
+
+        <div className="btn-branches" key="section-branches">
+          <button
+            key="menu-branch_1"
+            className={`btn btn-branch branch ${
+              !show || show === "branch_1" ? "active" : "inactive"
+            }`}
+            title="Show only branch 1"
+            onClick={() =>
+              toggleShow("branch_1" as Section)
+            }
+          >
+            <strong>{(info.branch_1 || { title: "" }).title}</strong>
+          </button>
+
+          <button
+            key="menu-branch_2"
+            className={`btn btn-branch branch ${
+              !show || show === "branch_2" ? "active" : "inactive"
+            }`}
+            title="Show only branch 2"
+            onClick={() =>
+              toggleShow("branch_2" as Section)
+            }
+          >
+            <strong>{(info.branch_2 || { title: "" }).title}</strong>
+          </button>
+
+          <button
+            key="menu-branch_3"
+            className={`btn btn-branch branch ${
+              !show || show === "branch_3" ? "active" : "inactive"
+            }`}
+            title="Show only branch 3"
+            onClick={() =>
+              toggleShow("branch_3" as Section)
+            }
+          >
+            <strong>{(info.branch_3 || { title: "" }).title}</strong>
+          </button>
+        </div>
+        <button
+          className={`btn btn-leaf leaf ${
+            !show || show === "leaf" ? "active" : "inactive"
+          }`}
+          title="Show only leaf"
+          onClick={() =>
+            toggleShow("leaf" as Section)
+          }
+          key="menu-leaf"
+        >
+          <strong>{(info.leaf || { title: "" }).title}</strong>
+          <small>{treeSections.leaf.length} articles</small>
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default TreeMenu;
