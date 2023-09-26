34dba126259ca20c3b7a6aac9f6000e96faf5cc9
Santiago Restrepo <santiagorv246@gmail.com>
2023-09-26 12:12:49 -0500

    Add pricing page

---
+++ src/components/pricing/constanst/index.ts
@@ -0,0 +1,14 @@
+export const includedFeaturesForBasic: string[] = [
+  "5 megabytes of capacity",
+  "No history",
+];
+
+export const includedFeaturesForFree: string[] = [
+  "10 megabytes of capacity",
+  "History of last 3 searches",
+];
+
+export const includedFeaturesForPro: string[] = [
+  "100 megabytes of capacity",
+  "Unlimited history",
+];
