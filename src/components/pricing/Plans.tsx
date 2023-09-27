7521c29aa28dffb5c29501357f3650ab1eff28cc
Oscar Arbelaez <oscar@arbelaez.dev>
2023-09-27 17:25:48 +0100

    Change plans a little bit

--- src/components/pricing/Plans.tsx
+++ src/components/pricing/Plans.tsx
@@ -14,7 +14,9 @@ const Plans: FC = () => {
     <Fragment>
       <div className="mx-auto max-w-7xl py-20 md:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-4xl font-bold tracking-tight text-gray-900 font-tall uppercase">Pricing</h2>
+          <h2 className="text-4xl font-bold tracking-tight text-gray-900 font-tall uppercase">
+            Pricing
+          </h2>
           <p className="mt-2 text-2xl leading-8">
             Choose one that's right for you.
           </p>
@@ -22,10 +24,14 @@ const Plans: FC = () => {
         <div className="flex flex-col mt-10 gap-5 lg:flex-row lg:justify-around">
           <div className="w-11/12 self-center ring-1 ring-inset ring-gray-300 flex flex-col justify-start md:w-8/12 lg:self-end lg:w-1/3">
             <div className="max-w-xs py-6 px-8">
-              <h1 className="font-tall font-bold text-4xl uppercase">Basic</h1>
-              <p className="mt-1">No need to register or pay, start creating your trees right now.</p>
+              <h1 className="font-tall font-bold text-4xl uppercase">Free</h1>
+              <p className="mt-1">
+                No need to register or pay, start creating your trees right now.
+              </p>
               <p className="mt-6 flex items-baseline gap-x-2">
-                <span className="text-2xl font-bold font-tall tracking-tight">Free</span>
+                <span className="text-2xl font-bold font-tall tracking-tight uppercase">
+                  Free
+                </span>
               </p>
               <div>
                 <ul
@@ -50,9 +56,14 @@ const Plans: FC = () => {
           <div className="w-11/12 self-center ring-1 ring-inset ring-gray-300 flex flex-col justify-start md:w-8/12 lg:self-end lg:w-1/3">
             <div className="max-w-xs py-6 px-8">
               <h1 className="font-tall font-bold text-4xl uppercase">Pro</h1>
-              <p className="mt-1">Contact us and we will understand your needs and give you the best offer that suits your needs.</p>
+              <p className="mt-1">
+                Contact us and we will understand your needs and give you the
+                best offer that suits your needs.
+              </p>
               <p className="mt-6 flex items-baseline gap-x-2">
-                <span className="text-2xl font-bold font-tall tracking-tight uppercase">Contact us</span>
+                <span className="text-2xl font-bold font-tall tracking-tight uppercase">
+                  Contact us
+                </span>
               </p>
               <div>
                 <ul
@@ -76,10 +87,15 @@ const Plans: FC = () => {
           </div>
           <div className="w-11/12 self-center ring-1 ring-inset ring-gray-300 flex flex-col justify-start md:w-8/12 lg:self-end lg:w-1/3">
             <div className="max-w-xs py-6 px-8">
-              <h1 className="font-tall font-bold text-4xl uppercase">Free</h1>
-              <p className="mt-1">You only need to register and start creating your trees, you don't need to pay anything.</p>
+              <h1 className="font-tall font-bold text-4xl uppercase">Basic</h1>
+              <p className="mt-1">
+                You only need to register and start creating your trees, you
+                don't need to pay anything.
+              </p>
               <p className="mt-6 flex items-baseline gap-x-2">
-                <span className="text-2xl font-bold font-tall tracking-tight">Free</span>
+                <span className="text-2xl font-bold font-tall tracking-tight uppercase">
+                  Free
+                </span>
               </p>
               <div>
                 <ul
