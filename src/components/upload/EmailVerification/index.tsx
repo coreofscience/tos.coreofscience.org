848867be3834f8dc2d4930794b657655e422b681
Santiago Restrepo <santiagorv246@gmail.com>
2023-10-11 09:46:33 -0500

    Add status to hide the notification

--- src/components/upload/EmailVerification/index.tsx
+++ src/components/upload/EmailVerification/index.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { sendEmailVerification } from "firebase/auth";
 import useFirebase from "../../../hooks/useFirebase";
 
 const EmailVerification: FC = () => {
  const firebase = useFirebase();
+ const [hidden, setHidden] = useState<boolean>(false);
 
  const handle = () => {
   const currentUser = firebase.auth.currentUser
@@ -11,17 +12,28 @@ const EmailVerification: FC = () => {
  }
 
  return (
-  <div className="flex flex-col gap-2 p-4 text-slate-50 bg-leaf">
-   <p>
-    Please verify your email.&nbsp;
-    <a
-     onClick={handle}
-     className="underline text-slate-50 hover:text-slate-200 cursor-pointer"
-    >
-     Resend email
-    </a>
-   </p>
-  </div>
+  !hidden ? (
+   <div className="flex flex-col gap-2 p-4 text-slate-50 bg-leaf">
+    <p>
+     Please verify your email.&nbsp;
+     <a
+      onClick={handle}
+      className="underline text-slate-50 hover:text-slate-200 cursor-pointer"
+     >
+      Resend email
+     </a>
+     .
+     &nbsp;
+     <a
+      onClick={() => setHidden(true)}
+      className="underline text-slate-50 hover:text-slate-200 cursor-pointer"
+     >
+      I already verified my email
+     </a>
+     .
+    </p>
+   </div>
+  ) : <></>
  );
 };
 
