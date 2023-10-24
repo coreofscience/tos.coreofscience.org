4ca34967f48d58ba491904fb733fb11b99ed0676
Oscar Arbelaez <oscar@arbelaez.dev>
2023-10-24 18:29:53 +0100

    Small adjustments to the hero section

--- src/components/pages/Home/Hero.tsx
+++ src/components/pages/Home/Hero.tsx
@@ -1,41 +1,43 @@
 import { FC } from "react";
 import UnderlinedIcon from "../../vectors/UnderlinedIcon";
-import PlayIcon from "../../vectors/PlayIcon";
 import { Link } from "react-router-dom";
 
 const Hero: FC = () => {
- return (
-  <section className="flex gap-8 flex-col">
-   <div className="flex gap-8 justify-center items-center">
-    <div className="flex flex-col gap-4 max-w-4xl items-center justify-center">
-     <h1 className="font-tall text-center max-w-4xl text-5xl sm:text-7xl">Accelerate your <span className="relative whitespace-nowrap text-leaf"><UnderlinedIcon /><span className="relative">literature</span></span> search</h1>
-     <p className="text-center mx-auto mt-6 max-w-2xl text-lg">Within minutes Tree of Science identifies and maps relevant academic literature for every research subject.</p>
-    </div>
-   </div>
-   <div className="flex flex-col sm:flex-row justify-center gap-3">
-    <Link
-     to="/tos"
-     className="px-4 py-2 font-tall text-center uppercase font-bold text-slate-50 bg-leaf"
-    >
-     Grow your tree now
-    </Link>
-    <Link
-     to="/tos"
-     className="px-4 py-2 font-tall text-center uppercase font-bold text-slate-50 bg-trunk"
-    >
-     Plant your seed files
-    </Link>
-    <a
-     href="https://www.youtube.com/@CoreofScience/videos"
-     target="_blank"
-     className="inline-flex items-center justify-center px-4 py-2 font-tall uppercase font-bold text-slate-50 bg-root"
-    >
-     <PlayIcon />
-     <span className="ml-3 w-max">Watch videos</span>
-    </a>
-   </div>
-  </section>
- );
+  return (
+    <section className="flex gap-8 flex-col">
+      <div className="flex gap-8 justify-center items-center">
+        <div className="flex flex-col gap-4 max-w-4xl items-center justify-center">
+          <h1 className="font-tall text-center max-w-4xl text-5xl sm:text-7xl">
+            Accelerate your{" "}
+            <span className="relative whitespace-nowrap text-leaf">
+              <UnderlinedIcon />
+              <span className="relative">literature</span>
+            </span>{" "}
+            search
+          </h1>
+          <p className="text-center mx-auto mt-6 max-w-2xl text-lg">
+            Within minutes Tree of Science identifies and maps relevant academic
+            literature for every research subject.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col sm:flex-row justify-center gap-3">
+        <Link
+          to="/tos"
+          className="px-4 py-2 font-tall text-center uppercase font-bold text-slate-50 bg-leaf"
+        >
+          Grow your tree now
+        </Link>
+        <a
+          href="https://www.youtube.com/@CoreofScience/videos"
+          target="_blank"
+          className="inline-flex items-center justify-center px-4 py-2 font-tall uppercase font-bold text-slate-50 bg-root"
+        >
+          Watch videos
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -5,23 +5,20 @@ import Pricing from "./Pricing";
 import TrustUs from "./TrustUs";
 import FAQ from "./FAQ";
 import GetStarted from "./GetStarted";
-import SecondaryFeatures from "./SecondaryFeatures";
 import PrimaryFeatures from "./PrimaryFeatures";
 
 const Home: FC = () => {
-
  return (
   <div className="flex flex-col gap-40">
    <Hero />
    <TrustUs />
    <PrimaryFeatures />
-   <SecondaryFeatures />
    <GetStarted />
    <Testimonials />
    <Pricing />
    <FAQ />
- </div>
- )
+  </div>
+ );
 };
 
 export default Home;
