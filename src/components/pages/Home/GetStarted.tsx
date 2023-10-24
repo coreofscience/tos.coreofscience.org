d5ea696dbfc80037b3a5e4ecbc730577a99270a9
Oscar Arbelaez <oscar@arbelaez.dev>
2023-10-24 19:23:26 +0100

    Adjust the get started section

--- src/components/pages/Home/GetStarted.tsx
+++ src/components/pages/Home/GetStarted.tsx
@@ -5,15 +5,14 @@ const GetStarted: FC = () => {
  return (
   <section className="mx-auto max-w-7xl">
    <div className="mx-auto max-w-lg text-center justify-center items-center flex flex-col gap-8">
-    <h2 className="text-3xl font-tall sm:text-4xl">
-     Get started today
-    </h2>
+    <h2 className="text-3xl font-tall sm:text-4xl">Get started today</h2>
     <p className="text-lg">
-     It’s time to take control of your books. Buy our software so you can feel like you’re doing something productive.
+     It's time to build your tree of science and accelerate your literature
+     search!
     </p>
     <Link
-    className="px-4 py-2 font-tall uppercase font-bold text-slate-50 bg-leaf"
-    to="/tos"
+     className="px-4 py-2 font-tall uppercase font-bold text-slate-50 bg-leaf"
+     to="/tos"
     >
      Start now
     </Link>
