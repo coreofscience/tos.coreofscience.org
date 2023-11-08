a739e1f5619828430a6ec0829353a7e6ece92047
Santiago Restrepo <santiagorv246@gmail.com>
2023-11-08 14:51:45 -0500

    Update HowItWorks section images

--- src/components/pages/Home/constanst/index.ts
+++ src/components/pages/Home/constanst/index.ts
@@ -1,9 +1,9 @@
 export const includedFeaturesForBasic: string[] = [
-  "Up to 10 megabytes file sizes",
+  "Up to 10 megabyte file sizes",
   "History of last 3 searches",
 ];
 
 export const includedFeaturesForPro: string[] = [
-  "Up to 100 megabytes file sizes",
+  "Up to 100 megabyte file sizes",
   "Unlimited history",
 ];
