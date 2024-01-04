e24f71cd496bd611354ba9e50d1da3caae42616c
Oscar Arbelaez <oscar@arbelaez.dev>
2024-01-04 06:26:48 +0000

    Remove unused comment

--- functions/main.py
+++ functions/main.py
@@ -1,7 +1,3 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 import logging
 from datetime import datetime
 from functools import reduce
