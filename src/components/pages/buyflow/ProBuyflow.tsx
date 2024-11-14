e421208257b14ad883f5e2da4c5df458de642d15
Oscar Arbelaez <oscar@arbelaez.dev>
2024-11-13 20:18:18 -0600

    Create an ugly buyflow

--- src/components/pages/buyflow/ProBuyflow.tsx
+++ src/components/pages/buyflow/ProBuyflow.tsx
@@ -3,6 +3,7 @@ import { Navigate } from "react-router-dom";
 
 const ProBuyflow = () => {
   const user = useUser();
+
   if (!user) {
     return (
       <Navigate
@@ -14,8 +15,27 @@ const ProBuyflow = () => {
     );
   }
   return (
-    <div>
-      <h1>ProBuyflow</h1>
+    <div className="flex flex-col gap-24">
+      <div className="flex justify-center">
+        <h2 className="text-center font-bold font-tall text-5xl sm:text-7xl">
+          Get Tree of Science Pro
+        </h2>
+      </div>
+      <div className="flex flex-col gap-4 items-center">
+        <div>
+          <p>With Tree of Science Pro you get:</p>
+          <ul className="list-disc list-inside">
+            <li>Upload files up to 100MB</li>
+            <li>Unlimited history</li>
+          </ul>
+          <p>For just $10/month</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <button className="rounded-sm bg-leaf px-4 py-2 text-center font-tall font-bold uppercase text-slate-50">
+          Buy now
+        </button>
+      </div>
     </div>
   );
 };
