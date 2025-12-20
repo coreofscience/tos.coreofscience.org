e8c7c7925b6c86f64275edb573ba1bb142ff8fb9
Oscar Arbelaez <oscar@arbelaez.dev>
2025-12-20 09:01:56 +0000

    chore: install tailwindcss

--- static/astro.config.mjs
+++ static/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
