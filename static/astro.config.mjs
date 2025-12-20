b70aa3fedfebd7b94291ac296eb1b5e3bc97c762
Oscar Arbelaez <oscar@arbelaez.dev>
2025-12-20 15:53:28 +0000

    chore: add shadcn

--- static/astro.config.mjs
+++ static/astro.config.mjs
@@ -4,10 +4,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
